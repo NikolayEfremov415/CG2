69cd1e03c134697f6b3c06ce490c0b7f12a1907d
TedyGroot <teoovcharov@gmail.com>
2026-03-05 01:30:02 +0200

    работещ портал

--- Assets/Sripts/SceneLoad.cs
+++ Assets/Sripts/SceneLoad.cs
@@ -3,22 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneTeleport : MonoBehaviour
 {
-    public string sceneToLoad = "Level1";
+    public string sceneToLoad;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.sceneLoaded += OnSceneLoaded;
             SceneManager.LoadScene(sceneToLoad);
         }
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        player.transform.position = new Vector3(0f, 0f, 0f);
-
-        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
